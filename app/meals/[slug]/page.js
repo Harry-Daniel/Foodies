d6639e4124e408adfad69557b91e8dfa4d833b83
Harry-Daniel <hayiborkomla@gmail.com>
2024-05-29 10:17:09 +0000

    Rendering Not Found States

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+import classes from './page.module.css'
 export default function MealDetailsPage({params}){
 return(
     <>
-    <h1>Meal Details</h1>
+   <header className={classes.header}>
+    <div className={classes.image}>
+        <Image fill/>
+    </div>
+    
+   </header>
+   <main></main>
     </>
 )
 }
